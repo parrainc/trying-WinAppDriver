2dca75bd4485bb7d36f33e976860310f1a33708f
Carlos Parra <carlos.parra.sbr@gmail.com>
2019-02-17 13:54:19 -0400

    playing with alarms and clocks app

---
+++ TryingWinAppDriver/AlarmsAndClockTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Support.UI;
+using TryingWinAppDriver.Sessions;
+
+namespace TryingWinAppDriver
+{
+    [TestClass]
+    public class AlarmsAndClockTests : AlarmsAndClockSession
+    {
+        private static WindowsElement menuElement;
+        private static WindowsElement bottomElement;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            // Create session to launch a Calculator window
+            Initialize(context);
+        }
+
+
+        [TestMethod]
+        public void AddNewAlarm()
+        {
+            session.FindElementByAccessibilityId("AddAlarmButton").Click();
+
+            //var editAlarmHeader = session.FindElementByName("NEW ALARM").Displayed;
+            //if (editAlarmHeader)
+            //{
+                // select the time: hour
+                var hourSelector = session.FindElementsByAccessibilityId("HourLoopingSelector");
+                //hourSelector.
+                ;
+                //var selectElement = new SelectElement(hourSelector);
+                //selectElement.SelectByValue("2");
+
+                // select the time: minute
+                var minuteSelector = session.FindElementByAccessibilityId("MinuteLoopingSelector");
+                var minSelectElement = new SelectElement(minuteSelector);
+                minSelectElement.SelectByValue("50");
+
+                // select the time: period AM/PM
+                var periodSelector = session.FindElementByAccessibilityId("PeriodLoopingSelector");
+                var periodSelectElement = new SelectElement(periodSelector);
+                periodSelectElement.SelectByValue("PM");
+                ;
+            //}
+        }
+
+
+        
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            CleanUp();
+        }
+    }
+}
---
+++ TryingWinAppDriver/Sessions/AlarmsAndClockSession.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Diagnostics;
+
+namespace TryingWinAppDriver.Sessions
+{
+    public class AlarmsAndClockSession
+    {
+        private const string WinAppDriverServerUrl = "http://127.0.0.1:4723";
+
+        // windows app has an app id, which is the PackageFamilyName+'!App'
+        private const string appId = "Microsoft.WindowsAlarms_8wekyb3d8bbwe!App";
+
+        // we just need this field to be accessed through inheritance
+        protected static WindowsDriver<WindowsElement> session;
+
+        public static void Initialize(TestContext testContext)
+        {
+            if (session is null)
+            {
+                var appCapabilities = new DesiredCapabilities();
+                appCapabilities.SetCapability("app", appId);
+                appCapabilities.SetCapability("deviceName", "WindowsPC");
+
+                // start the session/app
+                session = new WindowsDriver<WindowsElement>(new Uri(WinAppDriverServerUrl), appCapabilities);
+                Debug.Assert(session != null); // if the session could not be initialized, we can not continue...
+
+                // setting the timeout
+                session.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(2));
+            }
+        }
+
+        public static void CleanUp()
+        {
+            // Close the application and delete the session
+            if (session != null)
+            {
+                session.Quit();
+                session = null;
+            }
+        }
+    }
+}
